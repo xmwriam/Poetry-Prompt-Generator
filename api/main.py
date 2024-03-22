f2c5d996d8ca81626e4e37e3b760d22f824bc631
xmwriam <eqbalmariam@gmail.com>
2024-03-22 12:27:00 +0530

    tryingtofixmightnotwork

--- api/main.py
+++ api/main.py
@@ -14,11 +14,10 @@ def gen():
 
 @app.route("/prompt")
 def prompt():
-    headers={"User-Agent": "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"}
+    user_agent_list =['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',]
+    headers = {'User-Agent': user_agent_list[random.randint(0,2)]} 
     r = requests.get('https://api.reddit.com/r/WritingPrompts/', headers=headers)
     L =  r.json()
     num=len(L['data']['children'])
     ind=random.randint(3,num-1)
     return L['data']['children'][ind]['data']['title'][4::]
-
-#app.run(debug=True)
