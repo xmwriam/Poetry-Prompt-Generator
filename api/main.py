e11af349bff233f4354d970560a90b5e04af542f
xmwriam <eqbalmariam@gmail.com>
2024-03-31 23:54:04 +0530

    used praw instead of requests

--- api/main.py
+++ api/main.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask import render_template
-import requests
 import random
+import praw
+import os
 app = Flask(__name__)
 
 @app.route("/")
@@ -14,10 +15,15 @@ def gen():
 
 @app.route("/prompt")
 def prompt():
-    user_agent_list =['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',]
-    headers = {'User-Agent': user_agent_list[random.randint(0,2)]} 
-    r = requests.get('https://api.reddit.com/r/WritingPrompts/', headers=headers)
-    L =  r.json()
-    num=len(L['data']['children'])
-    ind=random.randint(3,num-1)
-    return L['data']['children'][ind]['data']['title'][4::]
+    reddit = praw.Reddit(
+    client_id=os.environ['clientID'],
+    client_secret=os.environ['clientSecret'],
+    user_agent=os.environ['userAgent'],
+)
+    prompts=[]
+    for submission in reddit.subreddit("WritingPrompts").hot(limit=10):
+        prompts.append(submission.title)
+    print(prompts)
+    num=len(prompts)
+    ind=random.randint(0,num-1)
+    return prompts[ind][4::]
